3c7f2a73494ee580192822e0f91056bfaca4b681
slwer <1047579002@qq.com>
2024-07-17 18:10:50 +0800

    feat: 实现网关过滤器

---
+++ course-zuul/src/main/java/com/slwer/course/filter/PreRequestFilter.java
@@ -0,0 +1,33 @@
+package com.slwer.course.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PreRequestFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 5;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext currentContext = RequestContext.getCurrentContext();
+        System.out.println("URI:" + currentContext.getRequest().getRequestURI());
+        return null;
+    }
+}
